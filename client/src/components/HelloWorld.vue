d9b13d734b4f9422a0e4562d3f60ecbe8ef8d0eb
Kanniyappan <kenz.mca86@gmail.com>
2023-06-03 00:12:59 +0530

    grid layout initiated

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>  
+    <div class="wrapper">
+      <div class="__header">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque nostrum harum, officia ad dolores excepturi voluptatum non nisi cupiditate consequuntur veritatis dicta vitae facilis nemo fuga accusantium sunt, omnis molestiae.
+      Unde incidunt dicta ratione illo. Quos perferendis, sit aliquid sed dolorem, consequuntur et repellendus voluptatum dolorum reiciendis doloremque, aliquam inventore nobis veniam! Blanditiis libero perspiciatis laudantium molestias excepturi debitis dolorum.
+      Magni culpa voluptas rerum autem pariatur, numquam amet ad dolor perferendis ut, eius quae veniam repellat maiores provident quidem recusandae laborum qui ipsam facilis consequuntur fugit dicta? Iusto, dicta quos?
+      Minima vero repudiandae provident quasi hic temporibus similique nisi facilis, unde cumque commodi dolorum, adipisci qui corrupti? Mollitia unde, molestiae nam eum iure nulla tempora minus. Aut consequuntur unde pariatur!
+      Numquam asperiores fugit hic magni voluptates, excepturi accusamus ea magnam libero sint praesentium rem aliquid esse saepe id soluta, laboriosam enim deleniti unde consequuntur architecto reprehenderit eveniet? Pariatur, inventore asperiores.</div>
+      <div class="__container">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia corporis beatae, animi tenetur eos sit doloribus repudiandae dolorum ullam cupiditate pariatur in. Doloremque repudiandae numquam voluptate qui corporis facere provident!</div>
+      <div class="__sidebar">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, asperiores! Eos sint ea officiis! Quisquam voluptas eveniet natus unde suscipit qui accusantium magnam, repellendus itaque dolor, doloribus quibusdam laudantium dolorem?</div>
+      <div class="__footer">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur atque, a fugit rerum iusto ad quis quae commodi quo aliquid dolorem possimus ab autem sint minima, quia esse suscipit ipsum?
+      Fugit veniam voluptates perferendis ab quos suscipit sequi iste, distinctio nihil beatae et! Laborum, autem odio consequatur similique vel, at ab assumenda provident cum, a natus tenetur perferendis incidunt hic.</div>
+    </div>
+    <!-- <div class="card">
+      <button type="button" @click="count++">count is {{ count }}</button>
+      <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test HMR
+      </p>
+    </div> -->
+</template>
+
+<style scoped>
+
+.wrapper {
+  display: grid;
+    grid-template-areas: "header header"
+      "sidebar main"
+      "footer footer";
+    grid-template-rows: 8rem 1fr;
+    grid-template-columns: 24rem 1fr;
+
+    @media (max-width: 425px) {
+      grid-template-areas: "header header"
+        "main main"
+        "footer footer";
+    }
+
+    @media (max-width: 375px) {
+      grid-template-rows: 6rem 1fr;
+    }
+  
+}
+
+.__header {
+  background-color: aquamarine;
+  grid-area: header;
+}
+.__container {
+  background-color: rgb(255, 150, 127);
+  grid-area: main;
+}
+.__sidebar {
+  background-color: rgb(240, 239, 241);
+  grid-area: sidebar;
+}
+.__footer {
+  background-color: rgb(158, 116, 136);
+  grid-area: footer;
+}
+.read-the-docs {
+  color: #888;
+}
+
+
+
+
+</style>
